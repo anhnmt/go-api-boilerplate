453e51eba35a30dae246b08f42f9a2325af80c26
anhnmt <anhng.dev@gmail.com>
2024-06-24 18:36:34 +0700

    Update base entity before create

--- internal/core/entity/base_entity.go
+++ internal/core/entity/base_entity.go
@@ -2,10 +2,35 @@ package entity
 
 import (
 	"time"
+
+	"github.com/google/uuid"
+	"gorm.io/gorm"
+	"gorm.io/gorm/callbacks"
 )
 
+var _ callbacks.BeforeCreateInterface = (*BaseEntity)(nil)
+
+// BaseEntity contains common columns for all tables.
 type BaseEntity struct {
-	ID        string    `gorm:"type:varchar(50);primaryKey;not null" json:"id"`
+	ID        string    `gorm:"type:uuid;primaryKey;not null" json:"id"`
 	CreatedAt time.Time `gorm:"type:timestamp(6) with time zone" json:"created_at"`
 	UpdatedAt time.Time `gorm:"type:timestamp(6) with time zone;index" json:"updated_at"`
+}
+
+// BeforeCreate will set a UUID rather than numeric ID.
+func (b *BaseEntity) BeforeCreate(*gorm.DB) error {
+	if b.ID == "" {
+		b.ID = uuid.New().String()
+	}
+
+	now := time.Now().UTC()
+	if b.CreatedAt.IsZero() {
+		b.CreatedAt = now
+	}
+
+	if b.UpdatedAt.IsZero() {
+		b.UpdatedAt = now
+	}
+
+	return nil
 }
--- internal/service/user/transport/grpc/grpc.go
+++ internal/service/user/transport/grpc/grpc.go
@@ -3,15 +3,12 @@ package usergrpc
 import (
 	"context"
 	"errors"
-	"time"
 
-	"github.com/google/uuid"
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/codes"
 	"google.golang.org/grpc/status"
 	"gorm.io/gorm"
 
-	"github.com/anhnmt/go-api-boilerplate/internal/core/entity"
 	userentity "github.com/anhnmt/go-api-boilerplate/internal/service/user/entity"
 	usercommand "github.com/anhnmt/go-api-boilerplate/internal/service/user/repository/postgres/command"
 	"github.com/anhnmt/go-api-boilerplate/proto/pb"
@@ -44,14 +41,7 @@ func (s *grpcService) ListUsers(context.Context, *pb.ListUsersRequest) (*pb.List
 }
 
 func (s *grpcService) CreateUser(ctx context.Context, req *pb.CreateUserRequest) (*pb.CreateUserReply, error) {
-	now := time.Now().UTC()
-
 	createUser := &userentity.User{
-		BaseEntity: entity.BaseEntity{
-			ID:        uuid.NewString(),
-			CreatedAt: now,
-			UpdatedAt: now,
-		},
 		Name:  req.Name,
 		Email: req.Email,
 	}
