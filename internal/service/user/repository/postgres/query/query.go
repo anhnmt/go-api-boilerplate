1730b73b15757fa876a86a6488646af30371452f
Anh Nguyen <anhng.dev@gmail.com>
2024-06-24 21:26:01 +0700

    Update list users

--- internal/service/user/repository/postgres/query/query.go
+++ internal/service/user/repository/postgres/query/query.go
@@ -1,15 +1,28 @@
 package userquery
 
 import (
-	"gorm.io/gorm"
+	"context"
+
+	"github.com/anhnmt/go-api-boilerplate/internal/infrastructure/gormgen"
+	userentity "github.com/anhnmt/go-api-boilerplate/internal/service/user/entity"
 )
 
 type Query struct {
-	db *gorm.DB
+	db *gormgen.Query
 }
 
-func New(db *gorm.DB) *Query {
+func New(db *gormgen.Query) *Query {
 	return &Query{
 		db: db,
 	}
+}
+
+func (q *Query) DB() *gormgen.Query {
+	return q.db
+}
+
+func (q *Query) ListUsers(ctx context.Context) ([]*userentity.User, error) {
+	e := q.DB().User
+
+	return q.db.WriteDB().User.WithContext(ctx).Select(e.ID, e.Name, e.Email, e.CreatedAt, e.UpdatedAt).Find()
 }
