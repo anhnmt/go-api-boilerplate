98e5e4752cfda1b42235817b2924feeed3084e86
anhnmt <anhng.dev@gmail.com>
2024-06-25 16:12:12 +0700

    Update auth proto validate

--- internal/service/auth/transport/grpc/grpc.go
+++ internal/service/auth/transport/grpc/grpc.go
@@ -4,8 +4,6 @@ import (
 	"context"
 
 	"google.golang.org/grpc"
-	"google.golang.org/grpc/codes"
-	"google.golang.org/grpc/status"
 
 	authbusiness "github.com/anhnmt/go-api-boilerplate/internal/service/auth/business"
 	"github.com/anhnmt/go-api-boilerplate/proto/pb"
@@ -31,5 +29,5 @@ func New(
 }
 
 func (s *grpcService) Login(context.Context, *pb.LoginRequest) (*pb.LoginReply, error) {
-	return nil, status.Errorf(codes.Unimplemented, "method Login not implemented")
+	return nil, nil
 }
