f6f6b32647c3ba11330365ff36659a3136c3404f
anhnmt <anhng.dev@gmail.com>
2024-07-19 12:41:06 +0700

    Refactor server

--- internal/server/server.go
+++ internal/server/server.go
@@ -6,6 +6,7 @@ import (
 	"fmt"
 	"net/http"
 	_ "net/http/pprof"
+	"time"
 
 	"connectrpc.com/vanguard"
 	"connectrpc.com/vanguard/vanguardgrpc"
@@ -14,7 +15,6 @@ import (
 	"go.uber.org/fx"
 	"golang.org/x/net/http2"
 	"golang.org/x/net/http2/h2c"
-	"golang.org/x/sync/errgroup"
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/encoding"
 	"google.golang.org/protobuf/encoding/protojson"
@@ -47,10 +47,6 @@ func init() {
 	}))
 }
 
-type Server interface {
-	Start(context.Context) error
-}
-
 type Params struct {
 	fx.In
 
@@ -59,13 +55,13 @@ type Params struct {
 	CryptoInterceptor cryptointerceptor.CryptoInterceptor
 }
 
-type server struct {
+type Server struct {
 	config config.Server
 	mux    http.Handler
 	srv    *http.Server
 }
 
-func New(lc fx.Lifecycle, p Params) (Server, error) {
+func New(lc fx.Lifecycle, p Params) (*Server, error) {
 	var mux http.Handler
 	var err error
 	mux, err = vanguardgrpc.NewTranscoder(p.GrpcServer, opts...)
@@ -79,59 +75,68 @@ func New(lc fx.Lifecycle, p Params) (Server, error) {
 	// Add CORS support
 	mux = withCORS(mux)
 
-	srv := &server{
+	srv := &Server{
 		mux:    mux,
 		config: p.Config,
 	}
 
-	lc.Append(fx.StartHook(srv.Start))
-	lc.Append(fx.StopHook(srv.Stop))
+	lc.Append(fx.StartStopHook(
+		srv.Start,
+		srv.Stop,
+	))
 
 	return srv, nil
 }
 
-func (s *server) Start(ctx context.Context) error {
-	g, _ := errgroup.WithContext(ctx)
+func (s *Server) Start() error {
+	errChan := make(chan error)
 
 	if s.config.Pprof.Enable {
-		g.Go(func() error {
+		go func(errChan chan error) {
 			addr := fmt.Sprintf(":%d", s.config.Pprof.Port)
 			log.Info().Msgf("Starting pprof http://localhost%s", addr)
 
-			return http.ListenAndServe(addr, http.DefaultServeMux)
-		})
+			errChan <- http.ListenAndServe(addr, http.DefaultServeMux)
+		}(errChan)
 	}
 
-	// Serve the http server on the http listener.
-	g.Go(func() error {
-		addr := fmt.Sprintf(":%d", s.config.Grpc.Port)
+	// create new http Server
+	addr := fmt.Sprintf(":%d", s.config.Grpc.Port)
+	s.srv = &http.Server{
+		Addr: addr,
+		// We use the h2c package in order to support HTTP/2 without TLS,
+		// so we can handle gRPC requests, which requires HTTP/2, in
+		// addition to Connect and gRPC-Web (which work with HTTP 1.1).
+		Handler: h2c.NewHandler(
+			s.mux,
+			&http2.Server{},
+		),
+	}
+
+	// Serve the http Server on the http listener.
+	go func(errChan chan error) {
 		log.Info().Msgf("Starting application http://localhost%s", addr)
 
-		// create new http server
-		s.srv = &http.Server{
-			Addr: addr,
-			// We use the h2c package in order to support HTTP/2 without TLS,
-			// so we can handle gRPC requests, which requires HTTP/2, in
-			// addition to Connect and gRPC-Web (which work with HTTP 1.1).
-			Handler: h2c.NewHandler(
-				s.mux,
-				&http2.Server{},
-			),
-		}
-
-		// run the server
+		// run the Server
 		err := s.srv.ListenAndServe()
 		if err != nil && !errors.Is(err, http.ErrServerClosed) {
+			errChan <- err
+		}
+	}(errChan)
+
+	select {
+	case err := <-errChan:
+		if err != nil {
 			return err
 		}
-
+	case <-time.After(100 * time.Millisecond):
 		return nil
-	})
+	}
 
-	return g.Wait()
+	return nil
 }
 
-func (s *server) Stop(ctx context.Context) error {
+func (s *Server) Stop(ctx context.Context) error {
 	if s.srv != nil {
 		return s.srv.Shutdown(ctx)
 	}
