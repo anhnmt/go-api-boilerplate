2306f21ece1c930b6ad80789cf85c447827e331b
anhnmt <anhng.dev@gmail.com>
2024-07-26 11:03:41 +0700

    Refactor fxlogger

--- internal/pkg/logger/fxlogger.go
+++ internal/pkg/logger/fxlogger.go
@@ -1,7 +1,6 @@
 package logger
 
 import (
-	"path/filepath"
 	"strings"
 
 	"github.com/rs/zerolog"
@@ -29,12 +28,12 @@ func (l *zeroLogger) LogEvent(event fxevent.Event) {
 	case *fxevent.OnStartExecuted:
 		if e.Err != nil {
 			l.log.Err(e.Err).
-				Str("function", getShortName(e.FunctionName)).
+				Str("function", e.FunctionName).
 				Str("caller", e.CallerName).
 				Msg("OnStart hook failed")
 		} else {
 			l.log.Info().
-				Str("function", getShortName(e.FunctionName)).
+				Str("function", e.FunctionName).
 				Str("caller", e.CallerName).
 				Str("runtime", e.Runtime.String()).
 				Msg("OnStart hook executed")
@@ -46,12 +45,12 @@ func (l *zeroLogger) LogEvent(event fxevent.Event) {
 	case *fxevent.OnStopExecuted:
 		if e.Err != nil {
 			l.log.Err(e.Err).
-				Str("function", getShortName(e.FunctionName)).
+				Str("function", e.FunctionName).
 				Str("caller", e.CallerName).
 				Msg("OnStop hook failed")
 		} else {
 			l.log.Info().
-				Str("function", getShortName(e.FunctionName)).
+				Str("function", e.FunctionName).
 				Str("caller", e.CallerName).
 				Str("runtime", e.Runtime.String()).
 				Msg("OnStop hook executed")
@@ -67,25 +66,26 @@ func (l *zeroLogger) LogEvent(event fxevent.Event) {
 				Msg("Supplied")
 		}
 	case *fxevent.Provided:
-		for _, rtype := range e.OutputTypeNames {
-			l.log.Info().
-				Str("type", rtype).
-				Str("constructor", getShortName(e.ConstructorName)).
-				Msg("Provided")
-		}
 		if e.Err != nil {
 			l.log.Err(e.Err).Msg("Error encountered while applying options")
+		} else {
+			for _, rtype := range e.OutputTypeNames {
+				l.log.Info().
+					Str("type", rtype).
+					Str("constructor", e.ConstructorName).
+					Msg("Provided")
+			}
 		}
 	// case *fxevent.Invoking:
 	// Do nothing. Will log on Invoked.
 	case *fxevent.Invoked:
 		if e.Err != nil {
 			l.log.Err(e.Err).
-				Str("function", getShortName(e.FunctionName)).
+				Str("function", e.FunctionName).
 				Msg("Invoke failed")
 		} else {
 			l.log.Info().
-				Str("function", getShortName(e.FunctionName)).
+				Str("function", e.FunctionName).
 				Msg("Invoked")
 		}
 	case *fxevent.Stopping:
@@ -113,9 +113,4 @@ func (l *zeroLogger) LogEvent(event fxevent.Event) {
 		//         l.log.Err(e.Err).Msg("custom logger initialization failed")
 		//     }
 	}
-}
-
-func getShortName(fullName string) string {
-	_, name := filepath.Split(fullName)
-	return strings.TrimSpace(name)
 }
